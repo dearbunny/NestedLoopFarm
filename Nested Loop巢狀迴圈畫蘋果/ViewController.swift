636ee010946e28d82c5bd082031fe5b3a9e5c1d0
Rose <qqbunny@icloud.com>
2021-05-18 15:52:51 +0800

    init

--- Nested Loop巢狀迴圈畫蘋果/ViewController.swift
+++ Nested Loop巢狀迴圈畫蘋果/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appleLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
+    // 變更數量
+    @IBAction func changeApple(_ sender: UISlider) {
+        var content = ""
+        sender.value = sender.value.rounded()
+        // rounded 變整數四捨五入 Slider才會停在整數的地方
+        let number = Int(sender.value)
+        numberLabel.text = "\(number)"
+        
+        for i in 1...number {
+            for _ in 1...number {
+                content += "🍎"
+            }
+            content += "\n"
+        }
+        appleLabel.text = content
+    }
+    
 
 
 }
-
